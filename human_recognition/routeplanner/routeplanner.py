8913f123fffd26197c549cf9a6f969abd715be02
RasitAkay11 <50198490+RasitAkay11@users.noreply.github.com>
2022-05-17 14:28:09 +0300

    Update routeplanner.py

--- human_recognition/routeplanner/routeplanner.py
+++ human_recognition/routeplanner/routeplanner.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python
 import time
 
-EdgeBL = [15,0]
-EdgeBR = [45,0]
-EdgeTL = [33,37]
-EdgeTR = [74,45]
+EdgeBL = [3,0] #Corner Bottom Left
+EdgeBR = [15,0] #Corner Bottom Right
+EdgeTL = [4,25] #Corner Top Left
+EdgeTR = [17,17] #Corner Top Right
 
-DistanceB = 0
-DistanceR = 0
-DistanceT = 0
-DistanceL = 0
+DistanceB = 0 #Distance bottom
+DistanceR = 0 #Distance right
+DistanceT = 0 #Distance top
+DistanceL = 0 #Distance left
 YDistanceT = 0
 YDistanceB = 0
 XDistanceR = 0
 XDistanceL = 0
-PointsPL = 2
-pointsR = []
-pointsL = []
+PointsPL = 2 #Stops per line
+YpointsR = []
+YpointsL = []
+XpointsR = []
+XpointsL = []
 
 def distancecalc():
     global EdgeBL
@@ -29,19 +31,10 @@ def distancecalc():
     global DistanceT
     global DistanceL
 
-    global YDistanceT
-    global YDistanceB
-    global XDistanceR
-    global XDistanceL
-
-
-    # EdgeTL     EdgeTR
-    #  .____.
-    #  |          |
-    #  |          |
-    #  |          |
-    #  .____.
-    # EdgeBL     EdgeBR
+    global YDistanceT #Difference between the Y values between EDGETL and EDGETR
+    global YDistanceB #Difference between the Y values between EDGEBL and EDGEBR
+    global XDistanceR #Difference between the X values between EDGEBR and EDGETR
+    global XDistanceL #Difference between the X values between EDGETL and EDGEBL
 
     DistanceB = EdgeBR[0] - EdgeBL[0]
     DistanceR = EdgeTR[1] - EdgeBR[1]
@@ -60,46 +53,69 @@ def distancecalc():
             XDistanceL = EdgeBL[0] - EdgeTL[0]
         else:
             XDistanceL = EdgeTL[0] - EdgeBL[0]
-        
+        print(XDistanceL)
         if EdgeTL[1] >= EdgeTR[1]:
             YDistanceT = EdgeTL[1] - EdgeTR[1]
         else:
             YDistanceT = EdgeTR[1] - EdgeTL[1]
-
+        print(YDistanceT)
         if EdgeTR[0] >= EdgeBR[0]:
             XDistanceR = EdgeTR[0] - EdgeBR[0]
         else:
             XDistanceR = EdgeBR[0] - EdgeTR[0]
-
+        print(XDistanceR)
         if EdgeBL[1] >= EdgeBR[1]:
             YDistanceB = EdgeBL[1] - EdgeBR[1]
         else:
             YDistanceB = EdgeBR[1] - EdgeBL[1]
+        print(YDistanceB)
+        print("")
+
 
 def routeplanner():
     global PointsPL
-    global pointsR
-    global pointsL
+    global YpointsR
+    global YpointsL
+    global XpointsR
+    global XpointsL
 
     global DistanceB
     global DistanceR
     global DistanceT
     global DistanceL
 
+    global YDistanceT
+    global YDistanceB
+    global XDistanceR
+    global XDistanceL
+
     counter = 0
-    amount = 0
     while counter != DistanceR:
         if counter % PointsPL == 0:
-            pointsR.append(counter)
-            pointsL.append(counter)
-            amount += 1
+            YpointsR.append(counter)
+        counter += 1
+    
+    if XDistanceL > 0:
+        AXL = XDistanceL / PointsPL
+        print(AXL)
+
+    counter = 0
+    while counter != DistanceL:
+        if counter % PointsPL == 0:
+            YpointsL.append(counter)
         counter += 1
 
-    print("The calculated Y values on line R are: ", pointsR) 
-    print("The calculated Y values on line L are: ", pointsL)
+    if XDistanceR > 0:
+        AXR = XDistanceR / PointsPL
+        print(AXR)
+
+    print("The calculated Y values on line R are: ", YpointsR) 
+    print("The calculated X values on line R are: ", XpointsR) 
+    print("The calculated Y values on line L are: ", YpointsL)
+    print("The calculated X values on line L are: ", XpointsL)
     print("")
 
 
 if _name=='main_':
     distancecalc()
-    #routeplanner()
+    routeplanner()
